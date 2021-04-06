710c975f621ab53c80a639cd914aff68f13e1961
Doctor Laplace <58324905+DoctorLaplace@users.noreply.github.com>
2021-04-06 15:26:19 -0700

    Nimbine forms crackle...

--- StormMind.cpp
+++ StormMind.cpp
@@ -19,118 +19,27 @@ void testGeneCross();
 int main(){
     srand(time(NULL));
 
-    Neuron a1, a2, b1, b2, b3, c1, c2;
-
-    a1.setActivity(1);
-    a2.setActivity(1);
-
-    a1.addConnection(&b1, 0.1);
-    a1.addConnection(&b2, 0.1);
-    a1.addConnection(&b3, 0.1);
-    a2.addConnection(&b1, 0.1);
-    a2.addConnection(&b2, 0.1);
-    a2.addConnection(&b3, 0.1);
-
-    b1.addConnection(&c1, 0.1);
-    b1.addConnection(&c2, 0.1);
-    b2.addConnection(&c1, 0.1);
-    b2.addConnection(&c2, 0.1);
-    b3.addConnection(&c1, 0.1);
-    b3.addConnection(&c2, 0.1);
-
-    Layer l1, l2, l3;
-    l1.addNeuronToLayer(&a1);
-    l1.addNeuronToLayer(&a2);
-
-    l2.addNeuronToLayer(&b1);
-    l2.addNeuronToLayer(&b2);
-    l2.addNeuronToLayer(&b3);
-
-    l3.addNeuronToLayer(&c1);
-    l3.addNeuronToLayer(&c2);
-
-    Membrane m1;
-
-    m1.addLayerToMembrane(&l1);
-    m1.addLayerToMembrane(&l2);
-    m1.addLayerToMembrane(&l3);
-
-    m1.forwardPropagateMembrane();
-    m1.displayMembrane();
+    Evolver zurel;
 
 
-    Neuron i1, i2, j1, j2, j3, k1, k2;
+    cout << "Minrae:\n";
+    std::vector<int> shapeA = {4,8,4,4};
+    Membrane* minrae = zurel.produceMembrane(shapeA, 0.5);
+    minrae->forwardPropagateMembrane();
+    minrae->displayMembrane();
 
-    i1.setActivity(1);
-    i2.setActivity(1);
+    cout << "Arkath:\n";
+    shapeA = {4,8,4,4};
+    Membrane* arkath = zurel.produceMembrane(shapeA, 0.1);
+    arkath->forwardPropagateMembrane();
+    arkath->displayMembrane();
 
-    i1.addConnection(&j1, 0.2);
-    i1.addConnection(&j2, 0.2);
-    i1.addConnection(&j3, 0.2);
-    i2.addConnection(&j1, 0.2);
-    i2.addConnection(&j2, 0.2);
-    i2.addConnection(&j3, 0.2);
-
-    j1.addConnection(&k1, 0.2);
-    j1.addConnection(&k2, 0.2);
-    j2.addConnection(&k1, 0.2);
-    j2.addConnection(&k2, 0.2);
-    j3.addConnection(&k1, 0.2);
-    j3.addConnection(&k2, 0.2);
-
-    Layer p1, p2, p3;
-    p1.addNeuronToLayer(&i1);
-    p1.addNeuronToLayer(&i2);
-
-    p2.addNeuronToLayer(&j1);
-    p2.addNeuronToLayer(&j2);
-    p2.addNeuronToLayer(&j3);
-
-    p3.addNeuronToLayer(&k1);
-    p3.addNeuronToLayer(&k2);
-
-    Membrane q1;
-
-    q1.addLayerToMembrane(&p1);
-    q1.addLayerToMembrane(&p2);
-    q1.addLayerToMembrane(&p3);
-
-    q1.forwardPropagateMembrane();
-    q1.displayMembrane();
+    Membrane* cylus = zurel.produceHybrid(minrae, arkath);
+    cylus->forwardPropagateMembrane();
+    cylus->displayMembrane();
 
 
-    Evolver iouna;
 
-    cout << "Vortex:\n";
-
-    Membrane* vortex;
-    vortex = iouna.geneCrossMembranes(&m1, &q1);
-    vortex->getMembraneVec()[0]->getLayerVec()[0]->setActivity(1);
-    vortex->getMembraneVec()[0]->getLayerVec()[1]->setActivity(1);
-    iouna.formMembraneConnections(vortex);
-    vortex->forwardPropagateMembrane();
-    vortex->displayMembrane();
-
-    cout << "Lance:\n";
-
-    Membrane* lance;
-    lance = iouna.geneCrossMembranes(&m1, &q1);
-    lance->getMembraneVec()[0]->getLayerVec()[0]->setActivity(1);
-    lance->getMembraneVec()[0]->getLayerVec()[1]->setActivity(1);
-    iouna.formMembraneConnections(lance);
-    lance->forwardPropagateMembrane();
-    lance->displayMembrane();
-    
-    cout << "Dromoskus:\n";
-
-    Membrane* dromoskus;
-    dromoskus = iouna.geneCrossMembranes(vortex, lance);
-    dromoskus->getMembraneVec()[0]->getLayerVec()[0]->setActivity(1);
-    dromoskus->getMembraneVec()[0]->getLayerVec()[1]->setActivity(1);
-    iouna.formMembraneConnections(dromoskus);
-    dromoskus->forwardPropagateMembrane();
-    dromoskus->displayMembrane();
-    
 
     return 0;
 }
--- Thunder.h
+++ Thunder.h
@@ -17,63 +17,64 @@ namespace Thunder{
 
     class Neuron {
 
-    public:
+        public:
 
-        struct connection{
-            Neuron* connectTarget = nullptr;
-            double connectionWeight = 0;
-        };
+            struct connection{
+                Neuron* connectTarget = nullptr;
+                double connectionWeight = 0;
+            };
 
-        void setActivity(double a){
-            activity = a;
-        }
-
-        double getActivity(){
-            return activity;
-        }
-
-        void addConnection(Neuron* target, double weight){
-            connection* newCon = new connection;
-            newCon->connectionWeight = weight;
-            newCon->connectTarget = target;
-            connectionVec.push_back(newCon);
-        }
-
-        void setConnectionTarget(int connectionIndex, Neuron* target){
-            connectionVec[connectionIndex]->connectTarget = target;
-        }
-
-        void forwardPropagate(){
-            for (size_t i = 0; i < connectionVec.size(); i++){
-                connectionVec[i]->connectTarget->activity += activity*connectionVec[i]->connectionWeight;
-            } 
-        }
-
-        void displayConnections(){
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, 11);
-            std::cout << "        Neuron: " << activity << std::endl;
-            for (size_t i = 0; i < connectionVec.size(); i++){
-                SetConsoleTextAttribute(hConsole, 9);
-                std::cout << "            Connection " << i << std::endl;
-                std::cout << "                Target: " << connectionVec[i]->connectTarget << std::endl;
-                std::cout << "                Weight: " << connectionVec[i]->connectionWeight << "\n";           
+            void setActivity(double a){
+                activity = a;
             }
-        }
 
-        std::vector<connection*> getConnectionVec(){
-            return connectionVec;
-        }
+            double getActivity(){
+                return activity;
+            }
 
-        void replaceConnections(std::vector<connection*> newConnections){
-            connectionVec = newConnections;
-        }
+            void addConnection(Neuron* target, double weight){
+                connection* newCon = new connection;
+                newCon->connectionWeight = weight;
+                newCon->connectTarget = target;
+                connectionVec.push_back(newCon);
+            }
+
+            void setConnectionTarget(int connectionIndex, Neuron* target){
+                connectionVec[connectionIndex]->connectTarget = target;
+            }
+
+            void forwardPropagate(){
+                for (size_t i = 0; i < connectionVec.size(); i++){
+                    //std::cout << "Impulse " << activity*connectionVec[i]->connectionWeight << " sent to: " << connectionVec[i]->connectTarget << "\n";
+                    connectionVec[i]->connectTarget->activity += activity*connectionVec[i]->connectionWeight;
+                } 
+            }
+
+            void displayConnections(){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 11);
+                std::cout << "        Neuron: " << activity << std::endl;
+                for (size_t i = 0; i < connectionVec.size(); i++){
+                    SetConsoleTextAttribute(hConsole, 9);
+                    std::cout << "            Connection " << i << std::endl;
+                    std::cout << "                Target: " << connectionVec[i]->connectTarget << std::endl;
+                    std::cout << "                Weight: " << connectionVec[i]->connectionWeight << "\n";           
+                }
+            }
+
+            std::vector<connection*> getConnectionVec(){
+                return connectionVec;
+            }
+
+            void replaceConnections(std::vector<connection*> newConnections){
+                connectionVec = newConnections;
+            }
 
 
-    private:
-        double activity = 0;
+        private:
+            double activity = 0;
 
-        std::vector<connection*> connectionVec;
+            std::vector<connection*> connectionVec;
 
 
 
@@ -83,75 +84,75 @@ namespace Thunder{
 
     class Layer {
 
-    private:
-        std::vector<Neuron*> layerVec;
+        private:
+            std::vector<Neuron*> layerVec;
 
 
-    public:
+        public:
+            
+            void addNeuronToLayer(Neuron *n){
+                layerVec.push_back(n);
+            }
+
+            std::vector<Neuron*> getLayerVec(){
+                return layerVec;
+            }
+
+            void forwardPropagateLayer(){
+                for (size_t i = 0; i < layerVec.size(); i++){
+                    layerVec[i]->forwardPropagate();
+                }
+            }
+
+            void displayLayer(){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 10);
+                std::cout << "    Layer: \n";
+                for (size_t i = 0; i < layerVec.size(); i++){
+                    layerVec[i]->displayConnections();
+                }
+                SetConsoleTextAttribute(hConsole, 15);
+
+            }
         
-        void addNeuronToLayer(Neuron *n){
-            layerVec.push_back(n);
-        }
-
-        std::vector<Neuron*> getLayerVec(){
-            return layerVec;
-        }
-
-        void forwardPropagateLayer(){
-            for (size_t i = 0; i < layerVec.size(); i++){
-                layerVec[i]->forwardPropagate();
-            }
-        }
-
-        void displayLayer(){
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, 10);
-            std::cout << "    Layer: \n";
-            for (size_t i = 0; i < layerVec.size(); i++){
-                layerVec[i]->displayConnections();
-            }
-            SetConsoleTextAttribute(hConsole, 15);
-
-        }
-     
     };
 
 
 
     class Membrane {
+        private:
 
-    private:
-        std::vector<Layer*> membraneVec;
+            std::vector<Layer*> membraneVec;
 
 
-    public:
-        void addLayerToMembrane(Layer *l){
-            membraneVec.push_back(l);
-        }
-
-        std::vector<Layer*> getMembraneVec(){
-            return membraneVec;
-        }
-
-        void setMembraneVec(std::vector<Layer*> newVec){
-            membraneVec = newVec;
-        }
-
-        void forwardPropagateMembrane(){
-            for (size_t i = 0; i < membraneVec.size(); i++){
-                membraneVec[i]->forwardPropagateLayer();
+        public:
+            void addLayerToMembrane(Layer *l){
+                membraneVec.push_back(l);
             }
-        }
 
-        void displayMembrane(){
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, 13);
-            std::cout << "Membrane: \n";
-            for (size_t i = 0; i < membraneVec.size(); i++){
-                membraneVec[i]->displayLayer();
+            std::vector<Layer*> getMembraneVec(){
+                return membraneVec;
             }
-            SetConsoleTextAttribute(hConsole, 15);
-        }
+
+            void setMembraneVec(std::vector<Layer*> newVec){
+                membraneVec = newVec;
+            }
+
+            void forwardPropagateMembrane(){
+                for (size_t i = 0; i < membraneVec.size(); i++){
+                    membraneVec[i]->forwardPropagateLayer();
+                }
+            }
+
+            void displayMembrane(){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 13);
+                std::cout << "Membrane: \n";
+                for (size_t i = 0; i < membraneVec.size(); i++){
+                    membraneVec[i]->displayLayer();
+                }
+                SetConsoleTextAttribute(hConsole, 15);
+            }
 
     };
 
@@ -159,113 +160,149 @@ namespace Thunder{
 
     class Evolver {
 
-    private:
-        Membrane* primeMembrane;
-        Membrane* secondaryMembrane;
+        private:
+            Membrane* primeMembrane;
+            Membrane* secondaryMembrane;
 
 
-    public:
+        public:
 
-        void setPrimeMembrane(Membrane* prime){
-            primeMembrane = prime;
-        }
+            void setPrimeMembrane(Membrane* prime){
+                primeMembrane = prime;
+            }
 
-        void setSecondaryMembrane(Membrane* secondary){
-            primeMembrane = secondary;
-        }
+            void setSecondaryMembrane(Membrane* secondary){
+                primeMembrane = secondary;
+            }
 
-        Neuron* geneCrossConnections(Neuron* n1, Neuron* n2){
-            std::vector<Thunder::Neuron::connection*> n1_connections = n1->getConnectionVec();
-            std::vector<Thunder::Neuron::connection*> n2_connections = n2->getConnectionVec();
-            Neuron* hybridNeuron = new Neuron;
+            Neuron* geneCrossConnections(Neuron* n1, Neuron* n2){
+                std::vector<Thunder::Neuron::connection*> n1_connections = n1->getConnectionVec();
+                std::vector<Thunder::Neuron::connection*> n2_connections = n2->getConnectionVec();
+                Neuron* hybridNeuron = new Neuron;
 
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, 11);
-            std::cout << "Performing neuron hybridization...  \n";
-            SetConsoleTextAttribute(hConsole, 15);
-
-            for (size_t i = 0; i < n1_connections.size(); i++){
-                Thunder::Neuron::connection*  newHybridConnection = new Thunder::Neuron::connection;
-
-                double n1ConWeight = n1_connections[i]->connectionWeight;
-                double n2ConWeight = n2_connections[i]->connectionWeight;
-                Neuron* n1ConTarget = nullptr; //n1_connections[i]->connectTarget;
-                Neuron* n2ConTarget = nullptr; //n2_connections[i]->connectTarget;
-                
-                // Random number between 1 and 100
-                double random_num = rand() % 100 + 1;
-                double random_weight = (rand() % 100 + 1);
-                random_weight = random_weight/10000;
-                random_weight = 0;
-
-                SetConsoleTextAttribute(hConsole, 9);
-                if (random_num >= 50){
-                    hybridNeuron->addConnection(n1ConTarget, n1ConWeight+random_weight);
-                    std::cout << "Type-1" << std::endl;
-                }else{
-                    hybridNeuron->addConnection(n2ConTarget, n2ConWeight+random_weight);
-                    std::cout << "Type-2" << std::endl;
-                }       
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 11);
+                std::cout << "Performing neuron hybridization...  \n";
                 SetConsoleTextAttribute(hConsole, 15);
-            }
-            // Return the created randomized neuron;
-            return hybridNeuron;
-        } 
 
-        Layer* geneCrossLayers(Layer* l1, Layer* l2){
-            std::vector<Neuron*> l1_neuron_vec = l1->getLayerVec();
-            std::vector<Neuron*> l2_neuron_vec = l2->getLayerVec();
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, 10);
-            std::cout << "Performing layer hybridization...  Size-" << l1_neuron_vec.size() << "\n";
-            SetConsoleTextAttribute(hConsole, 15);
-            Layer* newHybridLayer = new Layer;
-            for (size_t i = 0; i < l1_neuron_vec.size(); i++){
-                Neuron* newHybridNeuron = new Neuron;
-                newHybridNeuron = geneCrossConnections(l1_neuron_vec[i], l2_neuron_vec[i]);
-                newHybridLayer->addNeuronToLayer(newHybridNeuron);
-            }
+                for (size_t i = 0; i < n1_connections.size(); i++){
+                    Thunder::Neuron::connection*  newHybridConnection = new Thunder::Neuron::connection;
 
-            return newHybridLayer;
+                    double n1ConWeight = n1_connections[i]->connectionWeight;
+                    double n2ConWeight = n2_connections[i]->connectionWeight;
+                    Neuron* n1ConTarget = nullptr; //n1_connections[i]->connectTarget;
+                    Neuron* n2ConTarget = nullptr; //n2_connections[i]->connectTarget;
+                    
+                    // Random number between 1 and 100
+                    double random_num = rand() % 100 + 1;
+                    double random_weight = (rand() % 100 + 1);
+                    random_weight = random_weight/10000;
+                    random_weight = 0;
 
-        } 
+                    SetConsoleTextAttribute(hConsole, 9);
+                    if (random_num >= 50){
+                        hybridNeuron->addConnection(n1ConTarget, n1ConWeight+random_weight);
+                        std::cout << "Type-1" << std::endl;
+                    }else{
+                        hybridNeuron->addConnection(n2ConTarget, n2ConWeight+random_weight);
+                        std::cout << "Type-2" << std::endl;
+                    }       
+                    SetConsoleTextAttribute(hConsole, 15);
+                }
+                // Return the created randomized neuron;
+                return hybridNeuron;
+            } 
 
-        Membrane* geneCrossMembranes(Membrane* m1, Membrane* m2){
-            std::vector<Layer*> m1_layer_vec = m1->getMembraneVec();
-            std::vector<Layer*> m2_layer_vec = m2->getMembraneVec();
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, 13);
-            std::cout << "Performing membrane hybridization...  Depth-" << m1_layer_vec.size() << "\n";
-            SetConsoleTextAttribute(hConsole, 15);
-
-            Membrane* newHybridMembrane = new Membrane;
-            for (size_t i = 0; i < m1_layer_vec.size(); i++){
+            Layer* geneCrossLayers(Layer* l1, Layer* l2){
+                std::vector<Neuron*> l1_neuron_vec = l1->getLayerVec();
+                std::vector<Neuron*> l2_neuron_vec = l2->getLayerVec();
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 10);
+                std::cout << "Performing layer hybridization...  Size-" << l1_neuron_vec.size() << "\n";
+                SetConsoleTextAttribute(hConsole, 15);
                 Layer* newHybridLayer = new Layer;
-                newHybridLayer = geneCrossLayers(m1_layer_vec[i], m2_layer_vec[i]);
-                newHybridMembrane->addLayerToMembrane(newHybridLayer);
+                for (size_t i = 0; i < l1_neuron_vec.size(); i++){
+                    Neuron* newHybridNeuron = new Neuron;
+                    newHybridNeuron = geneCrossConnections(l1_neuron_vec[i], l2_neuron_vec[i]);
+                    newHybridLayer->addNeuronToLayer(newHybridNeuron);
+                }
+
+                return newHybridLayer;
+
+            } 
+
+            Membrane* geneCrossMembranes(Membrane* m1, Membrane* m2){
+                std::vector<Layer*> m1_layer_vec = m1->getMembraneVec();
+                std::vector<Layer*> m2_layer_vec = m2->getMembraneVec();
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 13);
+                std::cout << "Performing membrane hybridization...  Depth-" << m1_layer_vec.size() << "\n";
+                SetConsoleTextAttribute(hConsole, 15);
+
+                Membrane* newHybridMembrane = new Membrane;
+                for (size_t i = 0; i < m1_layer_vec.size(); i++){
+                    Layer* newHybridLayer = new Layer;
+                    newHybridLayer = geneCrossLayers(m1_layer_vec[i], m2_layer_vec[i]);
+                    newHybridMembrane->addLayerToMembrane(newHybridLayer);
+                }
+
+                return newHybridMembrane;
             }
 
-            return newHybridMembrane;
-        }
-
-
-        void formLayerToLayer(Layer* l1, Layer* l2){
-            for (size_t i = 0; i < l1->getLayerVec().size(); i++){
-                for (size_t k = 0; k < l2->getLayerVec().size(); k++){
-                    l1->getLayerVec()[i]->setConnectionTarget(k, l2->getLayerVec()[k]);
+            void formLayerToLayerConnections(Layer* l1, Layer* l2){
+                for (size_t i = 0; i < l1->getLayerVec().size(); i++){
+                    //std::cout << "   L-i:" << i << std::endl;
+                    for (size_t k = 0; k < l2->getLayerVec().size(); k++){
+                        //std::cout << "    L-k:" << k << std::endl;
+                        l1->getLayerVec()[i]->setConnectionTarget(k, l2->getLayerVec()[k]);
+                    }
                 }
             }
-        }
 
-        void formMembraneConnections(Membrane* m){
-            std::vector<Layer*> membraneVec = m->getMembraneVec();
-            //std::cout << membraneVec.size() << std::endl;
-            for (size_t i = 0; i < membraneVec.size() - 1; i++){
-                formLayerToLayer(membraneVec[i], membraneVec[i+1]);
+            void formMembraneConnections(Membrane* m){
+                std::vector<Layer*> membraneVec = m->getMembraneVec();
+                //std::cout << membraneVec.size() << std::endl;
+                for (size_t i = 0; i < membraneVec.size() - 1; i++){
+                    //std::cout << i << std::endl;
+
+                    formLayerToLayerConnections(membraneVec[i], membraneVec[i+1]);
+                }
             }
-        }
 
-        
+            Membrane* produceHybrid(Membrane* m1, Membrane* m2){
+                Membrane* newHybrid = new Membrane;
+                newHybrid = geneCrossMembranes(m1, m2);
+                formMembraneConnections(newHybrid);
+                return newHybrid;
+            }
+
+
+            Membrane* produceMembrane(std::vector<int> membraneShape, double startingWeight){
+                Membrane* newMembrane = new Membrane;
+
+                for (int i = 0; i < membraneShape.size(); i++){
+                   // std::cout << i << "Layer" << std::endl;
+                    Layer* newLayer = new Layer;
+                    newMembrane->addLayerToMembrane(newLayer);
+                    for (int k = 0; k < membraneShape[i]; k++){
+                        //std::cout << k << "Neuron" << std::endl;
+                        Neuron* newNeuron = new Neuron;
+                        newLayer->addNeuronToLayer(newNeuron);
+                        // If not the last layer
+                        if (i < membraneShape.size() - 1){
+                            for (int j = 0; j < membraneShape[i+1]; j++){
+                                //std::cout << j << "Connection" << std::endl;
+                                newNeuron->addConnection(newNeuron, startingWeight);
+                            }  
+                        }
+                    }
+                }
+
+                formMembraneConnections(newMembrane);
+
+                return newMembrane;
+            }
+
 
 
 
