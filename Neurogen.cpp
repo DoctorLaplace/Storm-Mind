5d77ca1edac6bec6cfc2241e0283bd6e5bf96f62
Doctor Laplace <58324905+DoctorLaplace@users.noreply.github.com>
2021-05-22 00:54:05 -0700

    Added comments and fixed axon mutation probability functions

--- Neurogen.cpp
+++ Neurogen.cpp
@@ -5,11 +5,13 @@
 #include <random>
 #include <windows.h>
 #include <iomanip>
+#include <algorithm>    // std::remove_if
 
 using namespace std;
 
 namespace Neurogen{
 
+    // Sigmoid function
     double sigmoid(double x){
         double value = x, e = 2.7182818284;
         value = pow(e, -x);
@@ -18,13 +20,13 @@ namespace Neurogen{
         return value;
     }
 
-
+    // Produces a random double between a specified minimum and maximum
     double randomDouble(double min, double max, int precision = 6){
         cout << setprecision(precision);
         return min + (double)(rand()) / ((double)(RAND_MAX/(max - min)));
     }
 
-
+    // Prints the contents of a vector of doubles to the terminal
     void printDVector(vector<double> v){
         for (int i = 0; i < v.size(); i++){
             cout << v[i] << endl;
@@ -52,6 +54,7 @@ namespace Neurogen{
             }
             ~neuron(){
                 // Deconstruct
+                // cout << "Neuron deleted <" << name << ">\n";
             }
 
             void normalize(){
@@ -105,6 +108,7 @@ namespace Neurogen{
             }
             ~axon(){
                 // Deconstruct
+                // cout << "Axon: " << name << " destroyed.\n";
             }
 
             void forwardActivation(){
@@ -135,6 +139,14 @@ namespace Neurogen{
 
         public:
             vector<axon*> axonVec;
+
+            ~axonSet(){
+                // Deconstruct
+                // cout << "Axon set deleted.\n";
+                for (int i = 0; i < axonVec.size(); i++){
+                    delete axonVec[i];
+                }
+            }
 
             axonSet* copyDeepAxonSet(){
                 axonSet* clone = new axonSet;
@@ -177,6 +189,8 @@ namespace Neurogen{
                 SetConsoleTextAttribute(hConsole, 1);
             }
 
+
+
     };
 
 
@@ -186,6 +200,15 @@ namespace Neurogen{
 
         public:
             vector<neuron*> neuronVec;
+
+            ~neuronLayer(){
+                // Deconstruct
+                // cout << "Neuron layer deleted.\n";
+                for (int i = 0; i < neuronVec.size(); i++){
+                    delete neuronVec[i];
+                }
+            }
+
 
             neuronLayer* copyDeepNeuronLayer(){
                 neuronLayer* clone = new neuronLayer;
@@ -237,6 +260,15 @@ namespace Neurogen{
 
             vector<axonSet*> completeLayer;
 
+
+            ~axonLayer(){
+                // Deconstruct
+                // cout << "Axon layer deleted.\n";
+                for (int i = 0; i < completeLayer.size(); i++){
+                    delete completeLayer[i];
+                }
+            }
+
             axonLayer* copyDeepAxonLayer(){
                 axonLayer* clone = new axonLayer;
 
@@ -280,7 +312,7 @@ namespace Neurogen{
     };
 
 
-
+    // An artificial neural network composed of neuron and axon layers
     class membrane{
 
         public:
@@ -288,12 +320,24 @@ namespace Neurogen{
             vector<neuronLayer*> neuronLayerVec;
             // Axon Layers contain axonSets
             vector<axonLayer*> axonLayerVec;
-
+            // The name of the membrane
             string name = "";
             
         public:
 
-            // Copies the shape, neurons, and activations of aneuron membrane
+            // Deconstructor for the membrane class
+            ~membrane(){
+                // Deconstruct
+                // cout << "Membrane " << name << " deleted.\n";
+                for (int i = 0; i < neuronLayerVec.size(); i++){
+                    delete neuronLayerVec[i];
+                }
+                for (int i = 0; i < axonLayerVec.size(); i++){
+                    delete axonLayerVec[i];
+                }
+            }
+
+            // Copies the shape, neurons, and activations of a neuron membrane
             membrane* copyDeepNeuronMembrane(){
                 membrane* clone = new membrane;
 
@@ -305,7 +349,7 @@ namespace Neurogen{
                 return clone;
             }
 
-            // Takes the membrane's existing axons and neurons, and weaves them in a dense architecture
+            // Sets the axon weights of this membrane identical to another membrane's
             void copyAxonWeights(membrane* target){
                 for(int i = 0; i < axonLayerVec.size(); i++){ 
                     for(int j = 0; j < axonLayerVec[i]->completeLayer.size(); j++){
@@ -316,22 +360,78 @@ namespace Neurogen{
                 }
             }
 
-            // Produces a copy of a dense network
+            // Sets the axon weights of this membrane to be a random composition inherited from two parent membranes
+            void copyGeneCrossAxonWeights(membrane* parent1, membrane* parent2){
+                for(int i = 0; i < axonLayerVec.size(); i++){ 
+                    for(int j = 0; j < axonLayerVec[i]->completeLayer.size(); j++){
+                        for(int k = 0; k < axonLayerVec[i]->completeLayer[j]->axonVec.size(); k++){
+                            int randomCoin = rand() % 2;
+                            double inheritedWeight;
+                            membrane* selectedParent;
+
+                            if (randomCoin == 0){
+                                selectedParent = parent1;
+                            }else{
+                                selectedParent = parent2;
+                            }
+
+                            axonLayerVec[i]->completeLayer[j]->axonVec[k]->weight = selectedParent->axonLayerVec[i]->completeLayer[j]->axonVec[k]->weight;
+                        }
+                    }
+                }
+            }
+
+            // Sets all of the axon weights within a membrane to a specific value
+            void setAllAxonWeights(double value){
+                for(int i = 0; i < axonLayerVec.size(); i++){ 
+                    for(int j = 0; j < axonLayerVec[i]->completeLayer.size(); j++){
+                        for(int k = 0; k < axonLayerVec[i]->completeLayer[j]->axonVec.size(); k++){
+                            axonLayerVec[i]->completeLayer[j]->axonVec[k]->weight = value;
+                        }
+                    }
+                }
+            }
+
+            // Mutates a percentage of the axons within a menmbrane
+            void mutatePartOfAxonWeights(double mutationRange, double percentMutated = 0.1){
+                // Rounds down to 2 decimal points by default
+                // To gain more p[recision a new technique would need development]
+                int probablityInt = percentMutated*100;
+                cout << probablityInt << endl;
+                int probabiliityCheck;
+                 
+                for(int i = 0; i < axonLayerVec.size(); i++){ 
+                    for(int j = 0; j < axonLayerVec[i]->completeLayer.size(); j++){
+                        for(int k = 0; k < axonLayerVec[i]->completeLayer[j]->axonVec.size(); k++){
+                            probabiliityCheck = rand() % 100;
+                            if (probabiliityCheck < probablityInt){
+                                axonLayerVec[i]->completeLayer[j]->axonVec[k]->mutateAxon(mutationRange);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Produces a clone of a dense network
             membrane* copyDenseMembrane(membrane* target){
                 membrane* clone = target->copyDeepNeuronMembrane();
                 clone->weaveDense();
                 clone->copyAxonWeights(target);
+                //clone->name = target->name + "-clone";
                 return clone;
             }
 
+            // Adds a neuron layer to the membrane
             void addNeuronLayer(neuronLayer* n){
                 neuronLayerVec.push_back(n);
             }
 
+            // Adds an axon layer to the membrane
             void addAxonLayer(axonLayer* a){
                 axonLayerVec.push_back(a);
             }
 
+            // Forward propagates activations through the membrane
             void forwardPropogateMembrane(bool normalizeFirstLayer = false){
 
                 if (normalizeFirstLayer == true){
@@ -345,18 +445,21 @@ namespace Neurogen{
 
             }
 
+            // Mutates all of the axons in the membrane by a defined random range
             void mutateAxonMembrane(double mutationRange){
                 for (int i = 0; i < axonLayerVec.size(); i++){
                     axonLayerVec[i]->mutateAxonLayer(mutationRange);
                 }
             }
 
+            // Sets all neuron activations to zero in the membrane
             void zeroNeuronMembrane(){
                 for (int i = 0; i < neuronLayerVec.size(); i++){
                     neuronLayerVec[i]->zeroNeuronLayer();
                 }
             }
 
+            // Displays the neuron activations within the membrane
             void displayMembrane(){
                 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
                 SetConsoleTextAttribute(hConsole, 13);
@@ -368,6 +471,17 @@ namespace Neurogen{
                 SetConsoleTextAttribute(hConsole, 1);
             }
 
+            // Displays the final neuron activations within the membrane
+            void displayMembraneFinalLayer(){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, 13);
+                cout << "   Membrane " << name << " Final Layer: \n";
+                    neuronLayer* currentNeuronLayer = neuronLayerVec[neuronLayerVec.size() - 1]; 
+                    currentNeuronLayer->displayNeurons();
+                SetConsoleTextAttribute(hConsole, 1);
+            }
+
+            // Displays the axon weights within the membrane
             void displayMembraneAxons(){
                 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
                 SetConsoleTextAttribute(hConsole, 13);
@@ -412,6 +526,7 @@ namespace Neurogen{
                 }
             }
 
+            // Creates a dense neural network from a given shape vector
             void createDense(vector<int> shape){
                 for (int i = 0; i < shape.size(); i++){
                     neuronLayer* newNeuronLayer = new neuronLayer;
@@ -426,6 +541,7 @@ namespace Neurogen{
                 //cout << "Axon Film Size: " << axonLayerVec.size() << endl;
             }
 
+            // Sets the activation of a specific neuron to a value
             void setNeuronActivity(int layerIndex, int neuronIndex, double value){
                 neuronLayerVec[layerIndex]->neuronVec[neuronIndex]->activation = value;
             }
@@ -437,6 +553,7 @@ namespace Neurogen{
                 }
             }
 
+            // Returns the activations of a particular layer as a vector of doubles
             vector<double> returnLayerActivity(int layerIndex){
                 vector<double> activity;
 
@@ -451,9 +568,19 @@ namespace Neurogen{
     };
 
 
+    // A system that trains membranes using various algorithms
     class architect{
 
         public:
+
+            vector<membrane*> deleteBucket;
+
+            void emptyDeleteBucket(){
+                for ( int i = 0; i < deleteBucket.size(); i++ ){       
+                    delete deleteBucket[i];    
+                }    
+                deleteBucket.clear(); 
+            }
 
             // Inputs data into the membrane and uses it to calculate and return output data.
             void computeMembrane(membrane* m, vector<double> input){
@@ -474,8 +601,6 @@ namespace Neurogen{
                 return error;
             }
 
-
-
             double supervisedSetError(membrane* m, vector<vector<double>> inputSet, vector<vector<double>> desiredOutputSet){
                 double totalError = 0;
 
@@ -489,18 +614,22 @@ namespace Neurogen{
             }
 
 
-             membrane* evolveSupervised(membrane* seedMembrane, vector<vector<double>> inputSet, vector<vector<double>> desiredOutputSet, double mutationRange = 0.1, int population = 5, int generations = 5){
+            membrane* evolveSupervised(membrane* seedMembrane, vector<vector<double>> inputSet, vector<vector<double>> desiredOutputSet, double mutationRange = 0.1, int population = 5, int generations = 5){
                 
                 
+                // Considers strongest membrane a clone of the seed membrane to avoid deleting it in the algorithm
+                membrane* strongestSpecimen = seedMembrane->copyDenseMembrane(seedMembrane);
                 vector<membrane*> genePool;
-                membrane* strongestSpecimen = seedMembrane;
+                genePool.push_back(strongestSpecimen);
+
 
                 // Use seed membrane to establish score baseline
-                genePool.push_back(strongestSpecimen);
                 double strongestScore = supervisedSetError(strongestSpecimen, inputSet, desiredOutputSet);
                 
 
                 for (int g = 0; g < generations; g++){
+
+                    
                     cout << "Generation " << g << "...\n";
                     for (int i = 0; i < population; i++){
                         membrane* newMembrane = strongestSpecimen->copyDenseMembrane(strongestSpecimen);
@@ -516,18 +645,109 @@ namespace Neurogen{
                             strongestScore = score;
                         }
                     }
-                    genePool.clear();
+
                     cout << "Membrane score: " << strongestScore << "\n\n";
+
+                    // Clean up
+                    // Add current genePool to delete bucket
+                    for (int k = 0; k < genePool.size(); k++){
+                        if (genePool[k] != strongestSpecimen){
+                            deleteBucket.push_back(genePool[k]);
+                        }
+                        
+                    }
+                    //cout << genePool.size() << endl;
+                    genePool.clear();                    
+                    emptyDeleteBucket();
 
                 }
 
+                cout << "Membrane score: " << strongestScore << "\n";
                 cout << "Training has ended...\n";
-
+                
+                
                 return strongestSpecimen;
 
             }
 
+            // Speciated Evolutionary Algorithm Recall
+            membrane* SEAR(membrane* seedMembrane, vector<vector<double>> inputSet, vector<vector<double>> desiredOutputSet, double mutationRange = 0.1, int population = 5, int generations = 5, double targetFitness = 0.1, int speciesCount = 2){
+
+                membrane* strongestSpecimen = seedMembrane->copyDenseMembrane(seedMembrane);
+                double strongestScore  = supervisedSetError(strongestSpecimen, inputSet, desiredOutputSet);
+
+                while (strongestScore > targetFitness){
+                    vector<membrane*> speciesList;
+
+                    for (int i = 0; i < speciesCount; i++){
+                        //cout << "Generating Species " << i << "\n";
+                        membrane* newSpecies = evolveSupervised(strongestSpecimen, inputSet, desiredOutputSet, mutationRange, population, generations);
+                        speciesList.push_back(newSpecies);
+                    }
+                    
+                    for (int i = 0; i < speciesList.size(); i++){
+                        double specimenScore = supervisedSetError(speciesList[i], inputSet, desiredOutputSet);
+                        if (specimenScore < strongestScore){
+                            strongestSpecimen = speciesList[i];
+                            strongestScore = specimenScore;
+                        }
+                    }
+
+                    // Clean up
+                    for (int k = 0; k < speciesList.size(); k++){
+                        if (speciesList[k] != strongestSpecimen){
+                            deleteBucket.push_back(speciesList[k]);
+                        }
+                        
+                    }
+                    speciesList.clear();                    
+                    emptyDeleteBucket();
+
+                    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                    SetConsoleTextAttribute(hConsole, 12);
+                    cout << "Entering next depth....\n";
+                    SetConsoleTextAttribute(hConsole, 1);
+                }
+
+                
+
             
+
+                cout << "Omega specimen found.\n";
+                cout << "Omega Score: " << strongestScore << endl;
+
+
+
+                return strongestSpecimen;
+
+
+
+
+
+            }
+
+            // Genetic evolution method
+            membrane* GEM(membrane* seedMembrane){
+                membrane* strongestMembrane = seedMembrane->copyDenseMembrane(seedMembrane);
+
+                // Rank the fitness of a group of membranes
+                // Select a percent of them based on fitness distribution
+                // Cross breed the selected
+                // Produce a new population of descendant membranes
+                // Mutate them
+                // Rank fitness
+
+
+
+
+
+
+
+
+
+                return strongestMembrane;
+            }
+
 
 
     };
--- StormMind.cpp
+++ StormMind.cpp
@@ -20,81 +20,24 @@ int main(){
     // Seed randomosity for mutations
     std::srand(std::time(nullptr));
 
-
-    // neuron a, b, c,
-    //         d, e, f;
-    
-    // axon    ab(&a, &b),
-    //         ac(&a, &c),
-    //         bd(&b, &d),
-    //         be(&b, &e),
-    //         bf(&b, &f),
-    //         cd(&c, &d),
-    //         ce(&c, &e),
-    //         cf(&c, &f)            
-    //         ;
-
-    // axonSet axonLayer1, axonLayer2;
-    // neuronLayer neuronLayer1, neuronLayer2, neuronLayer3;
-
-    // neuronLayer1.addNeuron(&a);
-    // axonLayer1.addAxon(&ab);
-    // axonLayer1.addAxon(&ac);
-    // neuronLayer2.addNeuron(&b);
-    // neuronLayer2.addNeuron(&c);
-    // axonLayer2.addAxon(&bd);
-    // axonLayer2.addAxon(&be);
-    // axonLayer2.addAxon(&bf);
-    // axonLayer2.addAxon(&cd);
-    // axonLayer2.addAxon(&ce);
-    // axonLayer2.addAxon(&cf);
-    // neuronLayer3.addNeuron(&d);
-    // neuronLayer3.addNeuron(&e);
-    // neuronLayer3.addNeuron(&f);
-
-
-
-    // membrane drem, knight;
-    // drem.addNeuronLayer(&neuronLayer1);
-    // drem.addAxonLayer(&axonLayer1);
-    // drem.addNeuronLayer(&neuronLayer2);
-    // drem.addAxonLayer(&axonLayer2);
-    // drem.addNeuronLayer(&neuronLayer3);
-
-
-
-    // a.activation = 1;
-    // ab.weight = 0.5;
-    // ac.weight = 1;
-    // bd.weight = 1;
-    // be.weight = 1;
-    // bf.weight = 1;
-    // cd.weight = 0.75;
-    // ce.weight = 0.5;
-    // cf.weight = 0.5;
-
-
-    // drem.forwardPropogateMembrane();
-    // drem.displayMembrane();
-    // axonLayer1.displayAxons();
-
     cout << "=================\n\n";
 
     architect ark;
+
 
     membrane everus;
     everus.name = "Everus";
 
 
-    vector<int> nnShape = {3, 8, 8, 1};
-    vector<double> imp1 = {0, 0, 1};
-    vector<double> imp2 = {1, 0, 1};
-    vector<double> imp3 = {0, 1, 1};
-    vector<double> imp4 = {1, 1, 1};
-    vector<double> des1 = {0};
-    vector<double> des2 = {1};
-    vector<double> des3 = {1};
-    vector<double> des4 = {0};
+    vector<int> nnShape = {3, 8, 8, 4};
+    vector<double> imp1 = {1, 0, 0, 0, 1};
+    vector<double> imp2 = {0, 1, 0, 0, 1};
+    vector<double> imp3 = {0, 0, 1, 0, 1};
+    vector<double> imp4 = {0, 0, 0, 1, 1};
+    vector<double> des1 = {0, 1, 0, 0};
+    vector<double> des2 = {0, 0, 1, 0};
+    vector<double> des3 = {0, 0, 0, 1};
+    vector<double> des4 = {1, 0, 0, 0};
     vector<vector<double>> imprintSet = {imp1, imp2, imp3, imp4};
     vector<vector<double>> desiredSet = {des1, des2, des3, des4};
 
@@ -102,7 +45,7 @@ int main(){
 
     everus.createDense(nnShape);
 
-    membrane* vera = ark.evolveSupervised(&everus, imprintSet, desiredSet, 0.2, 40, 300);
+    membrane* vera = ark.evolveSupervised(&everus, imprintSet, desiredSet, 0.5, 5, 100);
 
     ark.computeMembrane(vera, imp1);
     vera->displayMembrane();
@@ -113,69 +56,56 @@ int main(){
     ark.computeMembrane(vera, imp4);
     vera->displayMembrane();
 
+    cout << "=========NEXT MEM========\n\n";
 
-    cout << "=================\n\n";
-
-
-
-
-    // architect nebulus;
-
-    // membrane knight;
-    // knight.name = "Knight";
-
-
-    // vector<int> knightShape = {5, 3, 2};
-    // vector<double> imprint = {0.2, 0.5, 1, 0.4, 0.1};
-    // vector<double> output;
-    // knight.createDense(knightShape);
-    // nebulus.computeMembrane(&knight, imprint);
-    // output = knight.returnLayerActivity(2);
+    membrane zyg, wer, gon, zygwer;
+    zyg.createDense(nnShape);
+    zyg.setAllAxonWeights(1);
+    wer.createDense(nnShape);
+    wer.setAllAxonWeights(0);
+    gon.createDense(nnShape);
+    gon.setAllAxonWeights(-1);
+    zygwer.createDense(nnShape);
+    zygwer.copyGeneCrossAxonWeights(&zyg, &wer);
+    zygwer.copyGeneCrossAxonWeights(&zygwer, &gon);
+    zygwer.mutatePartOfAxonWeights(0.1, 0.1);
+    zygwer.displayMembraneAxons();
 
 
-    // printDVector(output);
-    // knight.mutateAxonMembrane(0.5);
-    // nebulus.computeMembrane(&knight, imprint);
-    // output = knight.returnLayerActivity(2);
-    // printDVector(output);
-    // knight.displayMembrane();
-    // knight.displayMembraneAxons();
+    //membrane* arev = ark.evolveSupervised(&everus, imprintSet, desiredSet, 0.5, 10, 10);
 
+    //membrane* alpheon = ark.SEAR(&everus, imprintSet,  desiredSet, 0.9, 40, 100, 0.1, 8);
 
+    //ark.computeMembrane(alpheon, imp1);
+    //alpheon->displayMembraneFinalLayer();
 
+    // NN seems to get caught in a local minimum quite often on XOR test.
+    // genetic cross over would help with this issue!
 
-    // cout << "========CLONE ZONE=========\n\n";
-
-    // membrane* witch = knight.copyDenseMembrane(&knight);
-    // witch->name = "witch";
-    // witch->mutateAxonMembrane(0.3);
-    // nebulus.computeMembrane(witch, imprint);
-    // output = witch->returnLayerActivity(2);
-    // witch->displayMembrane();
-    // witch->displayMembraneAxons();
-
-
-    // neuron a,b,c,d;
-    // a.name = "a";
-    // b.name = "b";
-    // c.name = "c";
-    // d.name = "d";
-
-    // neuron* e = a.copyNeuron();
-    // cout << e->name << endl;
-    // a.name = "a mod";
-    // cout << e->name << endl;
-    // cout << a.name << endl;
-    // e->name = "e";
-    // cout << e->name << endl;
-    // cout << a.name << endl;
 
 
     return 0;
 }
 
 
+/*  Notes
 
+    - Adding species restarts seems to help with falling into local minima.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 
 
 
